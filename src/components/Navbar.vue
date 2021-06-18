78ec55002486ad9a6d7cd764247c419348d233df
Sasha Berkowsky <s.nberkowsky@gmail.com>
2021-06-18 20:36:01 -0300

    navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,63 @@
+<template>
+  <section class="src-componentes-navbar">
+    <nav
+      class="navbar navbar-expand-lg navbar-light"
+      style="background-color: #e3f2fd"
+    >
+      <div class="container-fluid">
+        <a class="navbar-brand" href="/">Vacunatorio</a>
+
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" href="/pedirTurno">Pedi tu turno</a>
+            </li>
+          </ul>
+          <button class="btn btn-outline-info" type="submit">
+            Admin Login
+          </button>
+        </div>
+      </div>
+    </nav>
+  </section>
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'src-componentes-navbar',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+.src-componentes-navbar {
+}
+</style>
